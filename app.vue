cfd01247c7446a78abb840903a04578d6dce5c42
melvinprindustry <144001132+melvinprindustry@users.noreply.github.com>
2024-04-24 15:05:04 +0200

    Initial commit'

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+import * as Yup from 'yup'
+
+function handleSubmit(values) {
+  console.log(values);
+}
+
+const email = ref('test@test.')
+</script>
+
+<template>
+  <Form @submit="handleSubmit">
+    <label for="email"> Email: </label>
+    <InputField 
+      v-model="email"
+      name="email"
+      :rules="Yup.string().email().required()"
+    />
+    <ErrorMessage name="email" />
+    <button type="submit">Submit</button>
+  </Form>
+</template>
---
+++ components/InputField.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: [String],
+    default: "",
+  },
+  name: String,
+  rules: [String, Object, undefined]
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const name = toRef(props, 'name')
+const {
+  value: inputValue,
+  handleBlur,
+  handleChange
+} = useField(name, props.rules, { syncVModel: true })
+</script>
+
+<template>
+  <div>
+    <input 
+      :name="name"
+      :id="name"
+      type="text"
+      :value="inputValue"
+      @input="handleChange"
+      @blur="handleBlur"
+    >
+  </div>
+</template>
+
+<style scoped></style>
